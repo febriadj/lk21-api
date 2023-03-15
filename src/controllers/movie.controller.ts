87348ce7c5dc351d91fdba42d33c722fa0a7d358
febriadj <iamfebriadji@gmail.com>
2023-03-15 19:21:19 +0700

    feat(api): add popular movies

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -32,3 +32,34 @@ export const latestMovies: TController = async (req, res) => {
     res.status(500).json(null);
   }
 };
+
+/**
+ * Controller for `/popular/movies` route
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Next} next
+ */
+export const popularMovies: TController = async (req, res) => {
+  try {
+    const {
+      headers: { host },
+      query: { page },
+      protocol,
+    } = req;
+
+    const axiosRequest = await axios.get(
+      `${process.env.LK21_URL}/populer${
+        Number(page) > 1 ? `/page/${page}` : ''
+      }`
+    );
+
+    // scrape popular movies
+    const payload = await scrapeMovies(axiosRequest, { protocol, host });
+
+    res.status(200).json(payload);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json(null);
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router, IRouter } from 'express';
-import { latestMovies } from '../controllers/movie.controller';
+import { latestMovies, popularMovies } from '../controllers/movie.controller';
 
 const router: IRouter = Router();
 
 router.get('/movies', latestMovies);
+router.get('/popular/movies', popularMovies);
 
 export default router;
