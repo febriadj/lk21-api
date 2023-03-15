a4156ccf22f4de3ed9f372d6beb5769382c97c85
febriadj <iamfebriadji@gmail.com>
2023-03-15 19:40:03 +0700

    feat(api): add recent release movies

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -63,3 +63,34 @@ export const popularMovies: TController = async (req, res) => {
     res.status(500).json(null);
   }
 };
+
+/**
+ * Controller for `/recent-release/movies` route
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Next} next
+ */
+export const recentReleaseMovies: TController = async (req, res) => {
+  try {
+    const {
+      headers: { host },
+      query: { page },
+      protocol,
+    } = req;
+
+    const axiosRequest = await axios.get(
+      `${process.env.LK21_URL}/release${
+        Number(page) > 1 ? `/page/${page}` : ''
+      }`
+    );
+
+    // scrape popular movies
+    const payload = await scrapeMovies(axiosRequest, { protocol, host });
+
+    res.status(200).json(payload);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json(null);
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router, IRouter } from 'express';
-import { latestMovies, popularMovies } from '../controllers/movie.controller';
+import {
+  latestMovies,
+  popularMovies,
+  recentReleaseMovies,
+} from '../controllers/movie.controller';
 
 const router: IRouter = Router();
 
 router.get('/movies', latestMovies);
 router.get('/popular/movies', popularMovies);
+router.get('/recent-release/movies', recentReleaseMovies);
 
 export default router;
