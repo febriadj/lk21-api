d0dcc04c0cbde3ac4958b58872be23c2ca04b772
febriadj <iamfebriadji@gmail.com>
2023-07-07 17:18:38 +0700

    feat(api): add root

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import routes from './routes';
@@ -16,5 +16,15 @@ app.use(morgan('tiny'));
 app.use(cors({ origin: '*' }));
 
 app.use(routes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Unofficial LK21 (LayarKaca21) and NontonDrama APIs',
+    data: {
+      LK21_URL: process.env.LK21_URL,
+      ND_URL: process.env.ND_URL,
+    },
+  });
+});
 
 export default app;
